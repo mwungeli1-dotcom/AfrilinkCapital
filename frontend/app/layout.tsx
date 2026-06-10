5fc996b55cb5cded51c0421aae8a85c1c324692b
mwungeli1 <mwungeli1@gmail.com>
2026-06-10 13:58:21 +0200

    Add WhatsApp floating button

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "react-hot-toast";
+import WhatsAppButton from "@/components/WhatsAppButton";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,7 +29,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col"><Toaster />{children}</body>
+      <body className="min-h-full flex flex-col">
+  <Toaster />
+  {children}
+  <WhatsAppButton />
+</body>
     </html>
   );
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import WhatsAppButton from "@/components/WhatsAppButton";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
