815524fae5e7a1e4bc914e424af4db81557323a1
mwungeli1 <mwungeli1@gmail.com>
2026-07-02 12:21:09 +0200

    Add global footer

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -32,11 +32,12 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Header />
-        <Toaster />
-        {children}
-        <WhatsAppButton />
-      </body>
+  <Header />
+  <Toaster />
+  <main className="flex-1">{children}</main>
+  <Footer />
+  <WhatsAppButton />
+</body>
     </html>
   );
 }
